dd3edd65c6868e573beb36b32deffa987bcb0047
tung <darksteam_999@hotmail.com>
2015-08-17 11:05:20 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+// Load required modules
+var http    = require("http");              // http server core module
+var express = require("express");           // web framework external module
+var io      = require("socket.io");         // web socket external module
+var easyrtc = require("easyrtc");           // EasyRTC external module
+
+// Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
+var app = express();
+app.use(express.static(__dirname + '/static/'));
+
+// var httpApp = express();
+// httpApp.configure(function() {
+//     httpApp.use(express.static(__dirname + "/static/"));
+// });
+
+// Start Express http server on port 8080
+var webServer = http.createServer(app).listen(8080);
+
+// Start Socket.io so it attaches itself to Express server
+var socketServer = io.listen(webServer, {"log level":1});
+
+// Start EasyRTC server
+var rtc = easyrtc.listen(app, socketServer);
